263d1a5a489d56d1b54dd269cbf38428b565ceb9
luConcha <lucy.cm9@gmail.com>
2021-08-11 22:49:15 -0500

    commented console

--- examples/1-hello/app.js
+++ examples/1-hello/app.js
@@ -6,7 +6,7 @@ const fetchData = async () => {
     const { data } = await axios.get('/api/1-hello');
     result.textContent = data;
   } catch (error) {
-    console.log(error.response.data);
+    // console.log(error.response.data);
     result.textContent = error.response.data;
   }
 };
